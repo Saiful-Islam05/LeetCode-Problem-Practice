077a91493ad45f2e3e949997a72fe9e24ab9a78e
Saiful-Islam05 <132327157+Saiful-Islam05@users.noreply.github.com>
2026-05-18 21:39:27 +0600

    Time: 15 ms (44.01%) | Memory: 21.6 MB (45.84%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -1,41 +1,45 @@
 class Solution {
 public:
-    int dfs(int x,int y, vector<vector<int>> & matrix,vector<vector<int>>& dp)
+
+    int dfs(int x,int y,vector<vector<int>> &matrix,vector<vector<int>> &dp)
     {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
         if(dp[x][y]!=-1)
         {
             return dp[x][y];
         }
 
-        int n = matrix.size();
-        int m = matrix[0].size();
+        int curDis = 1;
 
         int dx[] = {0,0,1,-1};
         int dy[] = {1,-1,0,0};
 
-        int cur = 1;
-
         for(int i=0;i<4;i++)
         {
-            int a = dx[i]+x;
-            int b = dy[i]+y;
+            int a = x+dx[i];
+            int b = y+dy[i];
 
-            if(a>=0 && a<n && b>=0 && b<m && matrix[a][b]> matrix[x][y])
+            if(a>=0 && a<n && b>=0 && b<m && matrix[a][b]>matrix[x][y])
             {
-                cur = max(cur,dfs(a,b,matrix,dp)+1);
+                curDis = max(curDis,dfs(a,b,matrix,dp)+1);
             }
         }
 
-        return dp[x][y] = cur;
+        return dp[x][y] = curDis;
     }
+
+
     int longestIncreasingPath(vector<vector<int>>& matrix) {
+       
         int n = matrix.size();
         int m = matrix[0].size();
 
-        //dp.resize(n);
         vector<vector<int>> dp(n,vector<int>(m,-1));
 
-        int res = 0;
+        int res=0;
+
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
@@ -46,5 +50,6 @@ public:
         }
 
         return res;
+
     }
 };
